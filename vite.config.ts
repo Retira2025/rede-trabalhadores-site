1254a35ab1e71121551e074584370214a0c87bd1
Retira2025 <coord.retira@gmail.com>
2025-11-12 19:22:41 -0300

    modification

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Konfigirasyon Vite pou GitHub Pages
 export default defineConfig({
   base: '/',
   plugins: [react()],
 })
-
